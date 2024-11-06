4e75561437e028ed0db9f18f0cb93fc3370ab392
sosayessicaSE <dahiana-s@hotmail.com>
2024-11-06 11:45:06 -0300

    Fix Vite config for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./views/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import { keepTheme, setTheme } from "./components/mode";
 import Toggle from "./components/toggle";
@@ -35,7 +35,7 @@ function App() {
       <div className={`App ${themeClass}`}>
         <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         <Toggle isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-        
+
         <Navbar themeClass={themeClass} />
         <Routes>
           <Route
@@ -54,7 +54,7 @@ function App() {
               <Projects isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
             }
           />
-        <Route
+          <Route
             path="/experience"
             element={
               <Experience isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
@@ -67,7 +67,7 @@ function App() {
             }
           />
         </Routes>
-      
+
       </div>
     </Router>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace 'your-repo-name' with the actual name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
-  base: "/", // This is required for GitHub Pages
+  base: "/ysPortfolio/", 
 });
