994904b806ed210b7a004eeca8d42165e73e90aa
Yessica <dahiana-s@hotmail.com>
2026-05-06 16:12:18 -0300

    responsiveness

--- src/data/homeProjectGallerySlides.js
+++ src/data/homeProjectGallerySlides.js
@@ -1,18 +1,18 @@
 /**
  * Hero gallery on Home: Onix first, then each project's first screenshot in `projects.jsx` → `projectsData` order.
  */
-import splitsquids1 from '../images/projects/splitsquid1.png';
-import websitesprint1 from '../images/projects/websitesprint1.png';
-import safehalt1 from '../images/projects/safehalt1.png';
-import gp1 from '../images/projects/gp_1.png';
-import coding_temple1 from '../images/projects/ct_1.png';
-import yr1 from '../images/projects/yr_1.png';
-import ds1 from '../images/projects/ds_1.png';
-import onix2 from '../images/projects/onix2.png';
-import sunrise1 from '../images/projects/sunrise1.png';
 import cb1 from '../images/projects/cb1.png';
-import venus1 from '../images/projects/venus1.png';
+import coding_temple1 from '../images/projects/ct_1.png';
+import ds1 from '../images/projects/ds_1.png';
 import figuritas1 from '../images/projects/figuritas1.png';
+import gp1 from '../images/projects/gp_1.png';
+import jera2 from '../images/projects/je2.png';
+import onix2 from '../images/projects/onix2.png';
+import safehalt1 from '../images/projects/safehalt1.png';
+import splitsquids1 from '../images/projects/splitsquid1.png';
+import sunrise1 from '../images/projects/sunrise1.png';
+import websitesprint1 from '../images/projects/websitesprint1.png';
+import yr1 from '../images/projects/yr_1.png';
 
 /** @typedef {{ src: string; titleKey: string }} HomeProjectGallerySlide */
 
@@ -28,6 +28,6 @@ export const HOME_PROJECT_GALLERY_SLIDES = [
   { src: ds1, titleKey: 'projectsData.lavanderiadaserras.title' },
   { src: sunrise1, titleKey: 'projectsData.sunrise.title' },
   { src: cb1, titleKey: 'projectsData.codingBot.title' },
-  { src: venus1, titleKey: 'projectsData.venusSd.title' },
+  { src: jera2, titleKey: 'projectsData.jera.title' },
   { src: figuritas1, titleKey: 'projectsData.qatarCollection.title' },
 ];
